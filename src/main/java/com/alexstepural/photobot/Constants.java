6b2c34ffa5e2bcd5fd51b8c93948ada1ba1e98f8
Alexander Stepanov <alexstepural@gmail.com>
2018-07-20 14:22:13 +0500

    to deploy to heroku

--- src/main/java/com/alexstepural/photobot/Constants.java
+++ src/main/java/com/alexstepural/photobot/Constants.java
@@ -5,8 +5,10 @@ public final class Constants {
 	public static final String BOT_NAME="AlexrurBot";
 	public static final String BOT_TOKEN="611477743:AAF9kAZqPP4U4ct3cVsdWvL_Y_YQlA94-hk";
 	
-	private static final String PROXY_HOST="62.205.223.145";
+	/*
+	 * private static final String PROXY_HOST="62.205.223.145";
 	private static final Integer PROXY_PORT=8080;
 	private static final String PROXY_USER="";
 	private static final String PROXY_PASSWORD="";
+	*/
 }
